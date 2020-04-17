f117683409fe5039025d5332bf8987bce2ae68bb
Nikolay <arkhipov.na@phystech.edu>
2020-04-18 01:54:59 +0300

    release v5

--- pdp11_mem.c
+++ pdp11_mem.c
@@ -1,4 +1,4 @@
-#include <stdio.h>gcc
+#include <stdio.h>
 #include <assert.h>
 
 
@@ -8,7 +8,7 @@ typedef word Adress;  //16 bit
 
 #define MEMSIZE (64*1024)
 
-byte mem[MEMSIZE]; //массив типа byte размера MEMSZIE
+byte mem[MEMSIZE]; //массив типа byte размера MEMSIZE
 void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
@@ -19,7 +19,7 @@ void test_mem(){
     byte b0 = 0x0a;
     b_write(2, b0);
     byte bres =  b_read(2);
-    printf("02hhx == %02hhx",b0, bres);
+    printf("%02hhx == %02hhx\n",b0, bres);
 
 
     //Пишем 2 байта, читаем слово
@@ -30,8 +30,24 @@ void test_mem(){
     b_write(a, b0);
     b_write(a+1, b1);
     word wres = w_read(a);
-    printf("%04hx=%02hhx%02hhx\n", wres, b1, b0);
+    printf("%04hx == %02hhx%02hhx\n", wres, b1, b0);
     assert(w = wres);
+
+    // Пишем слово, читаем слово
+    word w1 = 0x0c0a;
+    w_write(6, w1);
+    word w1res = w_read(6);
+    printf("%04hx == %04hx\n", w1, w1res);
+    assert(w1 = wres);
+
+    // Пишем одно слово, читаем 2 байта
+    Adress a1 = 8;
+    word w2 = 0x0c0b;
+    w_write(a1, w2);
+    byte b2 = b_read(a1);
+    byte b3 = b_read(a1+1);
+    word w2res = w_read(a1);
+    printf("%02hhx%02hhx == %04hx\n", b3, b2, w2res);
 }
 
 int main() {
@@ -48,11 +64,14 @@ byte b_read(Adress adr){
 }
 
 void w_write(Adress adr, word w){
-    mem[adr] = w;
+    byte b1 = ((byte)(w >> 8));
+    byte b0 = ((byte)(w & 0xFF));
+    mem[adr] = b0;
+    mem[adr+1] = b1;
 }
 
 word w_read(Adress a){
-    word w =  ((word)mem[a+1]) << 8;//Сдвинули старший байт
+    word w =  ((word)mem[a+1]) << 8; //Сдвинули старший байт
     w = w | mem[a];
     printf("w = %x\n",w);
     return w;
--- pdp11_mem2.c
+++ pdp11_mem2.c
@@ -1,4 +1,73 @@
-//
-// Created by Николай Архипов on 17/04/2020.
-//
+#include <stdio.h>
+#include <assert.h>
+
+
+typedef unsigned char byte; //8 bit
+typedef unsigned short int word; //16 bit
+typedef word Adress;  //16 bit
+
+#define MEMSIZE (64*1024)
+
+word mem[MEMSIZE]; //массив типа byte размера MEMSZIE
+void b_write(Adress adr, byte b);
+byte b_read(Adress adr);
+void w_write(Adress adr, word w);
+word w_read(Adress adr);
+
+void test_mem(){
+    //Пишем байт, читаем байт
+    word w0 = 0x0b0a;
+    w_write(2, w0);
+    word wres =  w_read(2);
+    printf("04hx == %04hx",w0, wres);
+
+
+    //Пишем 2 байта, читаем слово
+    Adress a=4;
+    byte b1 = 0x0b;
+    byte b0 = 0x0a;
+    word w = 0x0b0a;
+    w_write(a, w);
+    byte b0res = b_read(a);
+    byte b1res = b_read(a+1);
+    printf("%04hx=%02hhx%02hhx\n", w, b1res, b0res);
+    assert(b0 = b0res);
+    assert(b1 = b1res);
+
+    // Пишем слово, читаем слово
+}
+
+int main() {
+    test_mem();
+    return 0;
+}
+
+void b_write(Adress adr,byte b){
+    if (adr % 2 == 0){
+        mem[adr] = b & 0xFF;
+    } else {
+        mem[adr-1] = ((word)(b << 8)) | mem[adr-1];
+    }
+}
+
+byte b_read(Adress adr){
+    byte b;
+    if (adr % 2 == 0){
+        b = mem[adr] & 0xFF;
+    } else {
+        word w = mem[adr-1];
+        b = w >> 8 & 0xFF;// байт по адрессу 5 мы достаем из слова по адрессу 4
+    }
+    return b;
+
+}
+
+void w_write(Adress adr, word w){
+    assert (adr % 2 == 0);
+    mem[adr] = w;
+}
+
+word w_read(Adress adr){
+    return mem[adr];
+}
 
